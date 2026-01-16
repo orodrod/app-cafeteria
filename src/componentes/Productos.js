f6ea6d4386ce73b29da769b276d6ae85e9fcc8d0
tarde <orodriguez1497@gmail.com>
2026-01-16 16:28:14 +0000

    estructuracion de la app

---
+++ src/componentes/Productos.js
@@ -0,0 +1,11 @@
+export const PRODUCTOS = [
+  { id: "agua", nombre: "Agua", precio: 1.5, img: "/img/Agua.png" },
+  { id: "refresco", nombre: "Refresco", precio: 2, img: "/img/Refresco.png" },
+  { id: "croissant", nombre: "Croissant", precio: 2.5, img: "/img/Croissant.png"  },
+  { id: "ensalada", nombre: "Ensalada", precio: 3.5, img: "/img/Ensalada.png"  },
+  { id: "hamburguesa", nombre: "Hamburguesa", precio: 3, img: "/img/Hamburguesa.png"  },
+  { id: "perrito", nombre: "Perrito", precio: 2.5, img: "/img/Perrito.png"  },
+  { id: "sandwich", nombre: "Sandwich", precio: 2.5, img: "/img/Sandwich.png"  },
+  { id: "postre", nombre: "Postre", precio: 3, img: "/img/Postre.png"  },
+  { id: "papas", nombre: "Paquete de Papas", precio: 1, img: "/img/Paquete_Papas.png"  },
+];
